4cf3089f4db07ce6923e454f45b3c2192b8af043
Muyedur rahman <muyedrahman732@gmail.com>
2025-11-09 00:17:00 +0600

    Setup Express server and connected MongoDB successfully

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.use(cors())
 app.use(express.json())
 
 // MongoDB
-// model-db   mKsvySoRkFPAfOzT
+// crop-db   yi8mul1AZNsyrNME
 const uri =
-  "mongodb+srv://model-db:mKsvySoRkFPAfOzT@cluster0.fkciokq.mongodb.net/?appName=Cluster0";
+  "mongodb+srv://crop-db:yi8mul1AZNsyrNME@cluster0.fkciokq.mongodb.net/?appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,10 +25,16 @@ async function run() {
   try {
     await client.connect();
     // API MongoDB  Data anbo and pathabo
+    const db = client.db("crop-db");
+    const cropsCollection = db.collection('crops')
+
+
+
+
 
     await client.db("admin").command({ ping: 1 });
     console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
+      "Pingeds your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
