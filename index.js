64b1d9e3c77400378a5aa15599765e9a673acffe
Muyedur rahman <muyedrahman732@gmail.com>
2025-11-12 23:27:14 +0600

    update

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
+const { MongoClient, ObjectId} = require("mongodb");
+
+
+
 
 const app = express();
 const port = 3000;
@@ -10,13 +13,9 @@ app.use(express.json());
 
 // mongoDB connection
 const uri = "mongodb+srv://crop-db:yi8mul1AZNsyrNME@cluster0.fkciokq.mongodb.net/?appName=Cluster0";
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+
+const client = new MongoClient(uri);
+
 
 async function run() {
   try {
@@ -24,7 +23,7 @@ async function run() {
     const db = client.db("crop-db");
     const cropsCollection = db.collection("crops");
 
-    //  Read all crops
+    //  read all crops
     app.get("/crops", async (req, res) => {
       const crops = await cropsCollection.find().toArray();
       res.send(crops);
@@ -36,9 +35,12 @@ async function run() {
       res.send(crop);
     });
     // submit
+    // submit interest
     app.post("/crops/:id/interests", async (req, res) => {
       const { id } = req.params;
       const interest = req.body;
+
+      // mongodb unique _id 
       const interestId = new ObjectId();
       const newInterest = { _id: interestId, ...interest };
 
@@ -47,11 +49,14 @@ async function run() {
         { $push: { interests: newInterest } }
       );
 
-      res.send(
-        result.modifiedCount > 0
-          ? { success: true, message: "Interest submitted successfully" }
-          : { success: false, message: "Failed to submit interest" }
-      );
+      res.send({
+        success: result.modifiedCount > 0,
+        message:
+          result.modifiedCount > 0
+            ? "Interest submitted successfully"
+            : "Failed to submit interest",
+        interestId: interestId.toString(), 
+      });
     });
 
     // update crop
@@ -109,7 +114,7 @@ async function run() {
 }
 
 run().catch(console.dir);
-
+// 5
 app.get("/", (req, res) => {
   res.send("Server is running fine!");
 });
